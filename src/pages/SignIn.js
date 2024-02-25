6a91baf7cbc6373152c981ba79ab0b2ae6100e48
skyminju9 <skyminju9@naver.com>
2024-02-25 23:04:01 +0900

    Sign in page 구현

--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -1,8 +1,17 @@
 import React from "react";
 import MyButton from "../components/MyButton";
+import MyInput from "../components/MyInput";
 import {ReactComponent as SmallLogo} from '../small_logo.svg';
+import { useNavigate } from "react-router-dom";
 
 const SignIn = () => {
+
+    const navigate = useNavigate();
+
+    const handleSignUp = () => {
+        navigate('/signup');
+    }
+
     return(
         <div className="SignIn">
             <div className="signin_logo">
@@ -15,6 +24,25 @@ const SignIn = () => {
                     TODOs <br/>
                     EVERYDAY <br/>
                 </h4>
+            </div>
+            <div className="signin_input_email">
+                <p>EMAIL ADDRESS</p>
+                <MyInput type={"id"} text={"userid@email.com"}/>
+            </div>
+            <div className="signin_input_password">
+                <p>PASSWORD</p>
+                <MyInput type={"password"} text={"password"}/>
+            </div>
+            <div className="signup_container">
+                <div className="signup_text">
+                    New to our website?
+                </div>
+                <div className="buttonContainer">
+                    <button className="signup_button" onClick={handleSignUp}>sign up</button>
+                </div>
+            </div>
+            <div className="signin_button">
+                <MyButton text="SIGN IN"/>
             </div>
         </div>
     )
