74c689cfaaeee96372d8e64e97a71347c3a1aebc
skyminju9 <skyminju9@naver.com>
2024-03-01 00:29:58 +0900

    handleSignIn, handleLogout 함수 생성

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
 import './App.css';
-import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import React, { useState } from 'react';
+import { BrowserRouter, Route, Routes, useNavigate } from 'react-router-dom';
 
 
 import MainPage from './pages/MainPage';
 import SignInPage from './pages/SignInPage';
 import SignUpPage from './pages/SignUpPage';
+import Home from './pages/Home';
 
 function App() {
+
+  const [LoggedIn, setLoggedIn] = useState(false);
+  
+
+  const handleSignIn = () => {
+    setLoggedIn(true);
+  }
+
+  const handleLogout = () => {
+    localStorage.remove('user');
+    setLoggedIn(false);
+  }
+
   return (
     <BrowserRouter>
       <div className="App">
         <Routes>
           <Route path='/' element={<MainPage/>}/>
-          <Route path='/signin' element={<SignInPage/>}/>
+          <Route path='/signin' element={<SignInPage onSignIn={handleSignIn}/>}/>
           <Route path='/signup' element={<SignUpPage/>}/>
+          <Route path='/home' element={<Home/>}/>  
         </Routes>
       </div>
     </BrowserRouter>
-    
   );
 }
 
