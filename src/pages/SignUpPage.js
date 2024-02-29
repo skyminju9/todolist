3043f6d8b3a4012514413e1172c667515e4051c3
skyminju9 <skyminju9@naver.com>
2024-03-01 00:29:18 +0900

    로컬 스토리지 - 회원가입 시 회원정보 저장

--- src/pages/SignUpPage.js
+++ src/pages/SignUpPage.js
@@ -1,10 +1,35 @@
-import React from "react";
+import React, { useState, useRef } from "react";
 import MyButton from "../components/MyButton";
 import MyInput from "../components/MyInput";
 import { useNavigate } from "react-router-dom";
 import { ReactComponent as SmallLogo} from '../small_logo.svg';
 
 const SignUpPage = () => {
+
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [checkPw, setCheckPw] = useState("");
+
+    const navigate = useNavigate();
+    
+    const handleSignUp = () => {
+        if(!email||!password||!checkPw){
+            alert('모든 항목을 입력하세요.');
+            return;
+        }
+        if(password === checkPw){
+            const user = {email, password};
+            localStorage.setItem(JSON.stringify(email), JSON.stringify(user));
+            alert('회원가입 성공! 로그인 화면으로 이동합니다.');
+            navigate('/signin');
+        }else{
+            alert('비밀번호를 다시 확인해주세요.');
+            return;
+        }
+    }
+
+
+
     return(
         <div className="SignUp">
             <div className="signup_logo">
@@ -20,17 +45,17 @@ const SignUpPage = () => {
             </div>
             <div className="signup_info">
                 <div className="signup_input_email">
-                    <MyInput title={"EMAIL ADDRESS"} type={"id"} text={"Enter Email Address"}/>
+                    <MyInput title={"EMAIL ADDRESS"} type={"id"} text={"Enter Email Address"} onChange={(e)=>setEmail(e.target.value)}/>
                 </div>
                 <div className="signup_input_password">
-                    <MyInput title={"PASSWORD"} type={"password"} text={"Enter Password"}/>
+                    <MyInput title={"PASSWORD"} type={"password"} text={"Enter Password"} onChange={(e)=>setPassword(e.target.value)}/>
                 </div>
                 <div className="signup_input_passwordcheck">
-                    <MyInput title={"CHECK PASSWORD"} type={"password"} text={"Check Password"}/>
+                    <MyInput title={"CHECK PASSWORD"} type={"password"} text={"Check Password"} onChange={(e)=>setCheckPw(e.target.value)}/>
                 </div>
             </div>
             <div className="signup_button">
-                <MyButton text="SIGN UP"/>
+                <MyButton text="SIGN UP" onClick={handleSignUp}/>
             </div>
         </div>
     )
