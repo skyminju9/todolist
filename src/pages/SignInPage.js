4890eff4190d51feb7a9cf58130c3f4b01073702
skyminju9 <skyminju9@naver.com>
2024-03-01 00:29:38 +0900

    로컬 스토리지 - 저장된 회원 정보를 이용하여 로그인 시 home 화면으로 이동하도록 처리

--- src/pages/SignInPage.js
+++ src/pages/SignInPage.js
@@ -1,12 +1,34 @@
-import React from "react";
+import React, {useState} from "react";
 import MyButton from "../components/MyButton";
 import MyInput from "../components/MyInput";
 import {ReactComponent as SmallLogo} from '../small_logo.svg';
 import { useNavigate } from "react-router-dom";
 
-const SignInPage = () => {
+const SignInPage = ({onSignIn}) => {
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
 
     const navigate = useNavigate();
+
+    const handleSignIn = () => {
+        console.log('handleSignIn Clicked');
+        console.log(email);
+        console.log(password);
+        
+
+        if(email && password){
+            const storedUser = JSON.parse(localStorage.getItem(JSON.stringify(email)));
+            console.log(storedUser);
+            if(storedUser.email === email && storedUser.password === password){
+                onSignIn();
+                alert('로그인 성공!');
+                navigate('/home');
+            }else{
+                alert('아이디 또는 비밀번호가 올바르지 않습니다.');
+            }
+        }
+    }
 
     const handleSignUp = () => {
         navigate('/signup');
@@ -27,10 +49,10 @@ const SignInPage = () => {
             </div>
             <div className="signin_info">
                 <div className="signin_input_email">
-                    <MyInput title={"EMAIL ADDRESS"} type={"id"} text={"userid@email.com"}/>
+                    <MyInput title={"EMAIL ADDRESS"} type={"id"} text={"userid@email.com"} onChange={(e)=>setEmail(e.target.value)}/>
                 </div>
                 <div className="signin_input_password">
-                    <MyInput title={"PASSWORD"} type={"password"} text={"password"}/>
+                    <MyInput title={"PASSWORD"} type={"password"} text={"password"} onChange={(e)=>setPassword(e.target.value)}/>
                 </div>
             </div>
             <div className="signup_container">
@@ -42,7 +64,7 @@ const SignInPage = () => {
                 </div>
             </div>
             <div className="signin_button">
-                <MyButton text="SIGN IN"/>
+                <MyButton text="SIGN IN" onClick={handleSignIn}/>
             </div>
         </div>
     )
