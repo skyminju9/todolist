66e46f543f2c2cda3f3b9510b18189dc0c60e27c
RetrieverOH <hyuk9833@naver.com>
2024-03-04 12:34:24 +0900

    기능구현

---
+++ src/component/TodoCard.js
@@ -0,0 +1,38 @@
+import { Rating } from "@mui/material";
+
+export const TodoCard = ({ todoData }) => {
+  const dateChange = (date) => {
+    const newDate = new Date(parseInt(todoData.date) * 1000);
+    return newDate.toLocaleTimeString("ko-KR", {
+      hour: "2-digit",
+      minute: "2-digit",
+    });
+  };
+
+  return (
+    <div className="TodoCard">
+      <div className="todo-top">
+        <div className="todo-top-left">
+          <div className="todo-title">{todoData.title}</div>
+          <div className="todo-rating">
+            <Rating
+              name="read-only"
+              value={parseFloat(todoData.rating)}
+              readOnly
+              size="small"
+            />
+          </div>
+        </div>
+        <div className="todo-top-right">
+          <div className="todo-date">{dateChange()}</div>
+        </div>
+      </div>
+      <div className="todo-bottom">
+        <div className="todo-content">{todoData.content}</div>
+        <div className="todo-tag">
+          <div className="todo-tag-text">{todoData.tag}</div>
+        </div>
+      </div>
+    </div>
+  );
+};
