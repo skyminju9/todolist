5a9729e87f9f85e54a8b6e29cc423d750fd1bddf
skyminju9 <skyminju9@naver.com>
2024-03-24 21:36:37 +0900

    Routes changed

--- src/App.js
+++ src/App.js
@@ -1,17 +1,32 @@
-import "./css/App.css";
-import Edit from "./pages/Edit";
+import React, {useState} from "react";
 
+import "./css/App.css";
+
+import Edit from "./pages/Edit";
 import Home from "./pages/Home";
 import Search from "./pages/Search";
+import MainPage from './pages/MainPage';
+import SignInPage from './pages/SignInPage';
+import SignUpPage from './pages/SignUpPage';
+import Setting from './pages/Search';
 
 import { Routes, Route } from "react-router-dom";
-import Setting from "./pages/Setting";
 
 function App() {
+  const [LoggedIn, setLoggedIn] = useState(false);
+  
+
+  const handleSignIn = () => {
+    setLoggedIn(true);
+  }
+
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path='/' element={<MainPage/>}/>
+        <Route path='/SignIn' element={<SignInPage onSignIn={handleSignIn}/>}/>
+        <Route path='/SignUp' element={<SignUpPage/>}/>
+        <Route path="/Home" element={<Home />} />
         <Route path="/Search" element={<Search />} />
         <Route path="/Edit" element={<Edit />} />
         <Route path="/Setting" element={<Setting />} />
